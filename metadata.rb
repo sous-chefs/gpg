89b94354a44267d2e4efa796a6dd9bfaa7499e80
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-21 15:02:03 -0800

    Update version. Add content to readme

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "AJ Christensen"
-maintainer_email "aj@junglist.gen.nz"
+maintainer       "Heavy Water"
+maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures gpg"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
