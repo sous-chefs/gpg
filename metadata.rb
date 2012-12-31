23e00d24503a330349b24c9f57dd61e4a74980bf
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-31 07:53:24 -0800

    Bump patch level 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures gpg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
