7d23a8f16685a253e6179c36967b2b96333198c3
AJ Christensen <aj@junglist.gen.nz>
2013-03-26 19:22:55 +1300

    chef 11 attributes, name metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name "gpg"
 maintainer       "Heavy Water"
 maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures gpg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
