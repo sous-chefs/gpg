0fbcf6b38cb0fd5efc18632a26b24870a1582e36
Tim Smith <tsmith@chef.io>
2018-05-08 19:12:46 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,9 +24,9 @@ if node['gpg']['override_default_keyring']
 end
 
 unless system("sudo -u #{node['gpg']['user']} -i gpg #{gpg_opts} --list-keys | grep \"#{node['gpg']['name']['real']}\"")
-  package "haveged"
+  package 'haveged'
 
-  service "haveged" do
+  service 'haveged' do
     supports [:status, :restart]
     action :start
   end
@@ -40,22 +40,22 @@ Name-Comment: #{node['gpg']['name']['comment']}
 Name-Email: #{node['gpg']['name']['email']}
 Expire-Date: #{node['gpg']['expire']['date']}
 EOS
-  if node['gpg']['override_default_keyring']
-    content << "%pubring #{node['gpg']['pubring_file']}\n"
-    content << "%secring #{node['gpg']['secring_file']}\n"
-  end
-  content << "%commit\n"
+    if node['gpg']['override_default_keyring']
+      content << "%pubring #{node['gpg']['pubring_file']}\n"
+      content << "%secring #{node['gpg']['secring_file']}\n"
+    end
+    content << "%commit\n"
   end
 
-  execute "gpg: generate" do
+  execute 'gpg: generate' do
     command "sudo -u #{node['gpg']['user']} -i gpg #{gpg_opts} --gen-key --batch #{node['gpg']['batch_config']}"
   end
 
-  service "haveged" do
+  service 'haveged' do
     action :stop
   end
 
-  package "haveged" do
+  package 'haveged' do
     action :purge
   end
 end
