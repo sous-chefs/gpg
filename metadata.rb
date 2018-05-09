a58e1f1a4861e792b3730b806e76311c77a8b886
Tim Smith <tsmith@chef.io>
2018-05-08 19:12:10 -0700

    Update metadata and add new fields
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
-name "gpg"
-maintainer       "Heavy Water"
-maintainer_email "support@hw-ops.com"
-license          "Apache 2.0"
-description      "Installs/Configures gpg"
+name             'gpg'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Installs/Configures gpg'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          '0.2.2'
+chef_version     '>= 12' if respond_to?(:chef_version)
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
+source_url 'https://github.com/sous-chefs/gpg'
+issues_url 'https://github.com/sous-chefs/gpg/issues'
