2f3961341175b60d7ed3f6762aea1bf6720b1b21
Dan Webb <dan.webb@damacus.io>
2018-07-24 10:50:19 +0100

    Use gpg2

--- resources/install.rb
+++ resources/install.rb
@@ -1,9 +1,11 @@
 property :name, String, default: '' # ~FC108
+property :install_extra_packages, [true,false], default: true
 
 action :install do
   include_recipe 'yum-epel' if platform_family?('rhel')
+  apt_update if platform_family?('debian')
 
-  package %w(haveged gnupg2)
+  package %w(haveged gnupg2) if new_resource.install_extra_packages
 
   service 'haveged' do
     supports [:status, :restart]
--- resources/key.rb
+++ resources/key.rb
@@ -8,7 +8,9 @@ property :name_email, String, default: lazy { "#{node.name}@example.com" }
 property :expire_date, String, default: '0'
 property :batch_name, String, name_property: true
 property :batch_config_file, String, default: lazy { ::File.expand_path("~#{user}/.gpg/gpg_batch_config_#{batch_name}") }
-property :key_type, String, default: '1', equal_to: %w(RSA 1 DSA 17 )
+property :user_config_file, String, default: lazy { ::File.expand_path("~#{user}/.gpg/gpg.conf") }
+property :agent_config_file, String, default: lazy { ::File.expand_path("~#{user}/.gpg/gpg-agent.conf") }
+property :key_type, String, default: '1', equal_to: %w(RSA 1 Elgamal 16 DSA 17 20 )
 property :key_length, String, default: '2048', equal_to: %w( 2048 4096 )
 property :passphrase, String, sensitive: true
 property :key_file, String
@@ -21,7 +23,7 @@ action :generate do
     unless ::Dir.exist?(config_dir)
       directory config_dir do
         owner new_resource.user
-        mode '0700'
+        mode '0600'
         recursive true
       end
     end
@@ -41,50 +43,74 @@ EOS
       end
 
       content << "Passphrase: #{new_resource.passphrase}" if new_resource.passphrase
-
       content << "%commit\n"
 
       mode '0600'
       owner new_resource.user
     end
 
-    cmd = "sudo -u #{new_resource.user} -i gpg2"
+    # The --with-colons option emits the output in a stable, machine-parseable format,
+    # which is intended for use by scripts and other programs
+
+    # --no-tty Make  sure that the TTY (terminal) is never used for any output.
+    # This option is needed in  some  cases  because  GnuPG  sometimes
+    # prints warnings to the TTY even if --batch is used.
+
+    # --full-gen-key is avilable after 2.0 (Centos 7 default)
+
+    file new_resource.agent_config_file do
+      user new_resource.user
+      content "allow-loopback-pinentry\n"
+      mode '0600'
+    end
+
+    cmd = "gpg2 "
     cmd << gpg_opts(new_resource) if new_resource.override_default_keyring
     cmd << ' --batch'
-    cmd << " --gen-key #{new_resource.batch_config_file}"
+    cmd << ' --no-tty'
+    cmd << ' --with-colons'
+    cmd << " --pinentry-mode 'loopback'"
+    cmd << " --status-fd '2'"
+    cmd << " --full-generate-key #{new_resource.batch_config_file}"
 
     execute 'gpg2: generate' do
       command cmd
       live_stream true
+      user new_resource.user
     end
-
   end
 end
 
 action :import do
   execute 'gpg2: import key' do
-    command "sudo -u #{new_resource.user} -i gpg2 --import #{new_resource.key_file}"
+    command "gpg2 --import #{new_resource.key_file}"
+    user new_resource.user
     not_if { key_exists(new_resource) }
   end
 end
 
 action :export do
   execute 'gpg2: export key' do
-    command "sudo -u #{new_resource.user} -i gpg2 --export -a \"#{new_resource.name_real}\" > #{new_resource.key_file}"
+    command "gpg2 --export -a \"#{new_resource.name_real}\" > #{new_resource.key_file}"
+    user new_resource.user
     not_if { ::File.exist?(new_resource.key_file) }
   end
+
+
 end
 
 action :delete_public_key do
   execute 'gpg2: delete key' do
-    command "sudo -u #{new_resource.user} -i gpg2 --batch --yes --delete-key \"#{new_resource.key_fingerprint}\""
+    command "gpg2 --batch --yes --delete-key \"#{new_resource.key_fingerprint}\""
+    user new_resource.user
     only_if { key_exists(new_resource) }
   end
 end
 
 action :delete_secret_keys do
   execute 'gpg2: delete key' do
-    command "sudo -u #{new_resource.user} -i gpg2 --batch --yes --delete-secret-keys \"#{new_resource.key_fingerprint}\""
+    command "gpg2 --batch --yes --delete-secret-keys \"#{new_resource.key_fingerprint}\""
+    user new_resource.user
     only_if { key_exists(new_resource) }
   end
 end
