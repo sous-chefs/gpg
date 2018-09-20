f8ef280df05df2b4451002c0036a3295a5665562
Bernhard Ott <bernhard.ott@westernacher.com>
2018-09-20 20:30:42 +0200

    added property home_dir to custom resource key; replaced sudo commands with shellout uid/gid and homedir option (fixes HOME issues), added gpg_cmd; added sensitive property to ; batch_config_file file resource; minor fixture changes

--- resources/install.rb
+++ resources/install.rb
@@ -5,7 +5,7 @@ action :install do
 
   package %w(haveged gnupg2)
 
-  service 'haveged' do
+  service 'haveged' do # FIXME: (?) this will create duplicates in case other cookbooks do the same
     supports [:status, :restart]
     action :start
   end
